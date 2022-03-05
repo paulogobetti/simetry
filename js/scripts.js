8b70ec841f8fe1383eb46e992e321535ed216494
Paulo Gobetti <paulogobetti@outlook.com.br>
2022-03-04 22:53:10 -0300

    Menu toggle button

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-function DarkMode() {
+function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+const menuButton = document.querySelector('.nav-btn');
+let menuOpen = false;
+menuButton.addEventListener('click', () => {
+
+  if(!menuOpen) {
+    menuButton.classList.add('open');
+    menuOpen = true;
+  }
+
+  else {
+    menuButton.classList.remove('open');
+    menuOpen = false;
+  }
+
+});
+ 
